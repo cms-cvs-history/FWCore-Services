5706a8e1cdc09c22e1d83658142ff7c93a4bd3bf
W.David Dagenhart <sha1-9ca965af9cf1904746014c29a94c3544e5eaff4b@cern.ch>
2006-12-18 16:24:06 +0000

    Add new feature that allows the service to set the precision of floating point calculations in x87 FPUs under LINUX to 64 bits (double) for addition, subtraction, multiplication, division, and square root.  By default this is enabled.  Also, change the defaults for floating point exceptions so that none are trapped by default.  We hope to change this back to enabled at some future point when exceptions occur less often in CMS code.

--- test/run_fpe.sh
+++ test/run_fpe.sh
@@ -3,15 +3,13 @@
 ulimit -c 0
 
 # Pass in name and status
-function die { echo $1: status $2 ;  exit $2; }
+# function die { echo $1: status $2 ;  exit $2; }
 
 F1=${LOCAL_TEST_DIR}/fpe_test.cfg
 
 cmsRun $F1 >& /dev/null
 status=$?
 if [ $status -ne 134 ] ; then
- die "Failure using $F1" $status
+ echo "Failure using $F1"
+ exit 1
 fi
-
-
-
