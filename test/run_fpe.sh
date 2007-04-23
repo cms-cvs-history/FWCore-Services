7074e18d3d445cb8cf80cf09c112d39e3fcd48ee
W.David Dagenhart <sha1-9ca965af9cf1904746014c29a94c3544e5eaff4b@cern.ch>
2007-04-23 18:35:07 +0000

    Modify test script to recognize 11 or 139 as successful return values.  I do not know why, but even on the same machine, same work area, same everything, when run repeatedly the job sometimes returns one and sometimes the other.  First noticed this on new SL4 machines, before it always returned 139.

--- test/run_fpe.sh
+++ test/run_fpe.sh
@@ -5,12 +5,12 @@ ulimit -c 0
 F1=${LOCAL_TEST_DIR}/fpe_test.cfg
 
 echo "***"
-echo "If the test is successful, cmsRun will fail (abort) with error status 139."
+echo "If the test is successful, cmsRun will fail (abort) with error status 139 or 11."
 echo "The purpose is to test that floating point exceptions cause failures."
 echo "If the floating point exception does not cause the cmsRun job to"
 echo "abort, an explicit exception will thrown from CMS code that also causes"
 echo "an abort, but this time with error status 65."
-echo "The values 139 and 65 depend on things underneath that CMS does not"
+echo "The values 139, 11, and 65 depend on things underneath that CMS does not"
 echo "control.  These values have changed before and may change again. If"
 echo "they do, someone will need to investigate and change the criteria in"
 echo "this shell script (run_fpe.sh)."
@@ -22,7 +22,7 @@ status=$?
 
 echo "Completed cmsRun $F1"
 echo "cmsRun status: " $status
-if [ $status -ne 139 ] ; then
+if [ $status -ne 139 -a $status -ne 11 ] ; then
  echo "Test FAILED, status not expected value"
  exit 1
 fi
