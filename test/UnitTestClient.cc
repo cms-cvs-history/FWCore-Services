a50960925c3579256852db6e902fa8565ab8d4e0
John Marraffino <sha1-3565b4bb138dc66a8251e997feee5430e8526679@cern.ch>
2007-09-09 18:01:46 +0000

    Rework the setting of the floating point control word to make fp exception handling a bit more transparent and robust.

--- test/UnitTestClient.cc
+++ test/UnitTestClient.cc
@@ -1,20 +1,46 @@
 #include "FWCore/Services/test/UnitTestClient.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include <iostream>
 #include <string>
 
+#include <cmath>
+#include <float.h>
+
 namespace edmtest
 {
-
-
 void
   UnitTestClient::analyze( edm::Event      const & e
                            , edm::EventSetup const & es )
 {
-  
-}  // MessageLoggerClient::analyze()
 
+   double x = 1.0;
+   double y = DBL_MAX;
+   edm::LogVerbatim("FPExceptions") << "\tx = " << x;
+   edm::LogVerbatim("FPExceptions") << "\ty = " << y;
+
+   // DivideByZero
+   edm::LogVerbatim("FPExceptions") << "\tForce DivideByZero";
+   double zero = 0.0;
+   double a = x / zero;
+   edm::LogVerbatim("FPExceptions") << "\ta = " << a;
+
+   // Invalid
+   edm::LogVerbatim("FPExceptions") << "\tForce Invalid";
+   double b = std::log(-1.0);
+   edm::LogVerbatim("FPExceptions") << "\tb = " << b;
+
+   // Overflow (actually precision)
+   edm::LogVerbatim("FPExceptions") << "\tForce Overflow";
+   double c = y * y;
+   edm::LogVerbatim("FPExceptions") << "\tc = " << c;
+
+   // Underflow (actually precision)
+   edm::LogVerbatim("FPExceptions") << "\tForce Underflow";
+   double d = x / y;
+   edm::LogVerbatim("FPExceptions") << "\td = " << d;
+}  
 
 }  // namespace edmtest
 
